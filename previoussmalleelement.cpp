9742fb81625e97750c291da1e61caf913c6bcb7f
thegod71 <143907160+thegod71@users.noreply.github.com>
2023-12-10 23:18:55 +0530

    Add files via upload

---
+++ previoussmalleelement.cpp
@@ -0,0 +1,31 @@
+#include<iostream> 
+#include<stack>
+#include<vector>
+using namespace std;
+vector<int>smaller(int arr[],int size,vector<int>ans){
+    stack<int>st;
+    st.push(-1);
+for (int i=0; i<size; i++)
+
+{
+int curr=arr[i];
+while(st.top()>=curr){
+    st.pop();
+}
+ans[i]=st.top();
+  st.push(curr); 
+}
+return ans;
+}
+int main(){
+    int arr[6]={3,5,8,2,1,9};
+    int size=6;
+ vector<int>ans(size);
+ ans =smaller(arr,size,ans);
+     for (int i = 0; i < 6; i++)
+     {cout<<ans[i]<<" ";
+        /* code */
+     }
+     
+    return 0;
+}
